959972e497107aac4bc67e88a42cb8987c936b23
rayahmian <rayah.mian@outlook.com>
2023-07-14 05:02:41 -0400

    Add movie_data_analysis.py

---
+++ movie_data_analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Initialize the data
+adf = pd.read_csv('amazonprime_data.csv')
+ddf = pd.read_csv('disneyplus_data.csv')
+hdf = pd.read_csv('hulu_data.csv')
+ndf = pd.read_csv('netflix_data.csv')
+
