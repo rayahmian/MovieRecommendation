0ee84c6ce287f183de2430e7b714cb7535cbfd07
Rayah Mian <rayah.mian@outlook.com>
2023-07-17 23:24:39 -0400

    Added views.py

--- recommender/views.py
+++ recommender/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
+def movie_list(request):
+    # Get all movies from the database
+    movies = Movie.objects.all()
+    return render(request, 'movie_list.html', {'movies': movies})
+
+def movie_detail(request, movie_id):
+    # Get the movie with the given ID from the database
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, 'movie_detail.html', {'movie': movie})
