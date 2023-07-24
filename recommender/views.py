e42b34b9a117173b36e721bd8520b33d0cf67ef6
rayahmian <rayah.mian@outlook.com>
2023-07-24 03:05:26 -0400

    Updated recommendations.html

--- recommender/views.py
+++ recommender/views.py
@@ -26,12 +26,13 @@ def recommend_movies(request):
     for genres in df['genre']:
         genre_choices.update(genre.strip() for genre in genres.split(','))
     for countries in df['country']:
-        if not isinstance(countries, float) and not pd.isna(countries):
-            country_choices.update(country.strip() for country in countries.split(','))
+        if isinstance(countries, str) and not pd.isna(countries):
+            countries_list = [country.strip() for country in countries.split(',') if country.strip()]
+            country_choices.update(countries_list)
 
     genre_choices = sorted(list(genre_choices))
     country_choices = sorted(list(country_choices))
-    release_year_choices = df['release_year'].unique()
+    release_year_choices = sorted(df['release_year'].unique(), reverse=True)
 
     if request.method == 'POST':
         form = MoviePreferencesForm(request.POST)
@@ -42,16 +43,20 @@ def recommend_movies(request):
             user_release_year = form.cleaned_data['release_year']
             user_country = form.cleaned_data['country']
 
+            # DEBUG
+            print(form.cleaned_data)
+
             # Logic to filter movies
             filtered_df = df.copy()
             if user_type:
                 filtered_df = filtered_df[filtered_df['type'] == user_type]
             if user_genre:
-                filtered_df = filtered_df[filtered_df['genre'].str.contains('|'.join(user_genre))]
+                filtered_df = filtered_df[filtered_df['genre'].apply(lambda x: any(item in x for item in user_genre))]
             if user_release_year:
-                filtered_df = filtered_df[filtered_df['release_year'] >= user_release_year]
-            if user_country:
-                filtered_df = filtered_df[filtered_df['country'] == user_country]
+                filtered_df = filtered_df[filtered_df['release_year'].isin(user_release_year)]
+            if user_country:  # Check if the user selected any countries
+                filtered_df = filtered_df[
+                    filtered_df['country'].apply(lambda x: any(item in x for item in user_country))]
 
             # Handle case when no movies match the user's criteria
             if filtered_df.empty:
@@ -61,7 +66,7 @@ def recommend_movies(request):
             # Logic to generate 5 random movies
             num_recommendations = 5
             recommendations = filtered_df.sample(n=num_recommendations)
-            recommendation_data = recommendations[['title', 'genre']]
+            recommendation_data = recommendations[['title', 'genre']].to_dict(orient='records')
             return render(request, 'recommendations.html', {
                 'recommendations': recommendation_data,
                 'user_type': user_type,
@@ -72,6 +77,9 @@ def recommend_movies(request):
                 'release_year_choices': release_year_choices,
                 'country_choices': country_choices
             })
+        else:
+            # DEBUG
+            print(form.errors)
     else:
         form = MoviePreferencesForm()
 
